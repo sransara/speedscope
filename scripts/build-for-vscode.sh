194c366be772cfea73e8c403d165cb20ac7f4db5
Samodya Abeysiriwardane <git@sransara.com>
2023-09-09 19:47:23 -0500

    Update the builder script to be runnable from anywhere

--- scripts/build-for-vscode.sh
+++ scripts/build-for-vscode.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR/"..  || exit 1
+
+# If `node_modules`` doesn't exist run `npm ci`
+# to install dependencies
+if [ ! -d "node_modules" ]; then
+  npm ci
+fi
+
 # Exact string `vscode-webview-url`
 # will be used by the VSCode extension to do a search and replace
 npx parcel build assets/index.html --public-url "./vscode-webview-url"
